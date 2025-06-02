13dd83c50c7395b3bc40ec1419559d0031243ac0
vishal-07 <dubeyvishal714@gmail.com>
2025-06-02 21:18:28 +0530

    navbar modified

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,15 @@
+import React from 'react';
+import {RiMenu3Line} from 'react-icons/ri';
+import logo from '../../assets/logo.png';
 const Navbar = ()=>{
     return(
-        <h1>Navbar</h1>
+        <div className= "gpt3__navbar">
+            <div className= "gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src={logo} alt = "logo"/>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Navbar;
